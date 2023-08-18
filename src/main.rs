12b87a6694f8efed83b70bad65b1caf7d0e63b02
PisonJay <PisonJay@outlook.com>
2023-08-18 19:34:02 +0800

    now generates aria2 input file with checksums

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,41 @@ struct Args {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+struct FileInfo {
+    hashes: HashInfo,
+
+    #[serde(rename = "mimeType")]
+    mime_type: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct HashInfo {
+    #[serde(rename = "sha1Hash")]
+    sha1_hash: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 struct DriveItemResp {
     id: String,
     name: String,
     folder: Option<Value>,
     children: Option<Vec<DriveItemResp>>,
+    file: Option<FileInfo>,
+    // #[serde(rename = "@content.downloadUrl")]
+    // download_url: Option<String>,
+}
+
+fn print_aria2_entry<'a>(base_url: &'a str, prefix: &'a str, entry: &'a DriveItemResp) {
+    let url = format!("{}/items/{}/content", base_url, &entry.id);
+    let path = format!("{}{}", prefix, &entry.name);
+    let sha1_hash = &entry.file.as_ref().unwrap().hashes.sha1_hash;
+    println!(
+        "
+{}
+    out={}
+    checksum=sha-1={}
+",
+        url, path, sha1_hash
+    );
 }
 
 fn visit<'a>(
@@ -29,7 +59,7 @@ fn visit<'a>(
 ) -> BoxFuture<'a, MyResult> {
     async move {
         let resp: DriveItemResp = reqwest::get(format!(
-            "{}/items/{}/?expand=children(select=id,name,folder,children)",
+            "{}/items/{}/?expand=children(select=id,name,children,file,folder)",
             &base_url, &id
         ))
         .await?
@@ -39,7 +69,7 @@ fn visit<'a>(
         if resp.folder.is_some() {
             let new_prefix = format!("{}{}/", prefix, resp.name);
 
-            let children = &resp.children.unwrap();
+            let children = resp.children.as_ref().unwrap();
 
             let mut tasks: FuturesUnordered<_> = children
                 .iter()
@@ -47,7 +77,7 @@ fn visit<'a>(
                     if child.folder.is_some() {
                         Some(visit(client, base_url, &child.id, &new_prefix))
                     } else {
-                        println!("{}{}", &new_prefix, child.name);
+                        print_aria2_entry(base_url, &new_prefix, child);
                         None
                     }
                 })
@@ -57,7 +87,7 @@ fn visit<'a>(
                 ret?;
             }
         } else {
-            println!("{}{}", &prefix, resp.name);
+            print_aria2_entry(base_url, prefix, &resp);
         }
 
         Ok(())
